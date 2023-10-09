abb67e4486469fce4d611eb2156b0ced74fd3927
dzlagit <146720843+dzlagit@users.noreply.github.com>
2023-10-09 14:04:02 +0100

    Update exerciseSix.java

--- exerciseSix.java
+++ exerciseSix.java
@@ -5,8 +5,14 @@ public class Main
     double a = 0; // Area
     Scanner input = new Scanner(System.in); // Create a Scanner object
     System.out.println("What is the length of the side of your hexagon? "); // Prompt user for length of side of hexagon
-    double s = input.nextInt(); // Read user input
-    a = (6 * (s * s)) / (4 * Math.tan(Math.PI / 6)); // Calculate area of hexagon
-    System.out.println("Your area is: " + a); // Print area of hexagon
+    double s = input.nextDouble(); // Read user input
+    
+    if (s == (double)s) {
+        a = (6 * (s * s)) / (4 * Math.tan(Math.PI / 6)); // Calculate area of hexagon
+        System.out.println("Your area is: " + a); // Print area of hexagon
+    } else {
+        System.out.println("You have not entered a valid input for hexagon side size.");
+            }
     }
 }
+
