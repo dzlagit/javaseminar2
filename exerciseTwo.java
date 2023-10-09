a0f436fa04fdb15d344720216a246fc9cb05a153
dzlagit <146720843+dzlagit@users.noreply.github.com>
2023-10-09 13:32:07 +0100

    Update exerciseTwo.java

--- exerciseTwo.java
+++ exerciseTwo.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args)  {
-		Scanner number1 = new Scanner(System.in); //creates a new var for input
+		Scanner input = new Scanner(System.in); //creates a new var for input
 		System.out.println("Enter your first number: "); //prints input prompt
-		int numberOne = number1.nextInt(); //assigns numberOne to input
+		int numberOne = input.nextInt(); //assigns numberOne to input
 		Scanner number2 = new Scanner(System.in); //creats a new var for input
 		System.out.println("Enter your second number: "); //prints input prompt
-		int numberTwo = number2.nextInt(); //assigns numberTwo to input
+		int numberTwo = input.nextInt(); //assigns numberTwo to input
 		
 		System.out.println(numberOne + numberTwo); //prints the results of numbers
 	}
